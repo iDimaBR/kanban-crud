17a7271efadf5835ae54cc3e66fec4e2873a5ca9
Raphael Alexei <raphaeeelbr@gmail.com>
2023-12-03 09:38:11 -0300

    feat: novas rotas adicionadas
    Rotas de categoria e tarefa adicionadas ao projeto

---
+++ routes/category.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const categoryController = require("../controllers/categoryController");
+
+router.get("/", categoryController.getAllCategories);
+router.post("/create", categoryController.createCategory);
+router.put("/:id", categoryController.updateCategory);
+router.delete("/:id", categoryController.deleteCategory);
+
+module.exports = router;
