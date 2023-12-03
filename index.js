b60b719506ca0a84616568f5bba065bd6d271e49
Raphael Alexei <raphaeeelbr@gmail.com>
2023-12-03 13:11:16 -0300

    fix: Tratamento de conexão
    Implementado um tratamento na conexão do mongodb.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const connection = require("./connection");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+connection()
+  .then(() => {
+    console.log('Conectado ao servidor MongoDB');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar ao MongoDB:', error);
+  });
 
 app.use("/category", require("./routes/category"));
 app.use("/task", require("./routes/task"));
