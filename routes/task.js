f038edb624a54362663befd34d29197fe84d329a
Raphael Alexei <raphaeeelbr@gmail.com>
2023-12-03 13:13:16 -0300

    feat: Implementação das rotas
    Foi implementada as rotas de categoria e tarefas.

--- routes/task.js
+++ routes/task.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const taskController = require("../controllers/taskController");
+const taskController = require("../controllers/TaskController");
 
 router.get("/", taskController.getAllTasks);
+router.get("/:id", taskController.findTask);
 router.post("/create", taskController.createTask);
 router.put("/:id", taskController.updateTask);
 router.delete("/:id", taskController.deleteTask);
